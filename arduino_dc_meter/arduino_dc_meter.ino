0f31cb8f3d99670dd84532b7fe156fbc508eb81f
nuhundred@gmail.com <nuhundred@gmail.com>
2019-08-29 17:19:50 +0700

    add circuit

--- arduino_dc_meter/arduino_dc_meter.ino
+++ arduino_dc_meter/arduino_dc_meter.ino
@@ -13,18 +13,20 @@ float R2 = 20000.0; // resistance of R2 (20K)
 int value = 0;
 void setup() {
   pinMode(analogInput, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(115200);
 }
 void loop() {
   // read the value at analog input
+  digitalWrite(LED_BUILTIN, HIGH);
   value = analogRead(analogInput);
   vout = (value * 5.0) / 1024.0;
-  vin = vout / (R2 / (R1 + R2));
+  vin = vout / (R2 / (R1 + R2)); //Voltage divider equation
   if (vin < 0.09) {
+    digitalWrite(LED_BUILTIN, LOW);
     vin = 0.0; //statement to quash undesired reading !
   }
   Serial.print("INPUT V= ");
   Serial.println(vin);
-  delay(1000);
-
+  delay(500);
 }
